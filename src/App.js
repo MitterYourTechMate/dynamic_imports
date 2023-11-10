d6ccaa9797a8e3ca44e5650381644cc3243b8d8f
shankey raheja <shankeyraheja@gmail.com>
2023-11-10 17:09:33 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {lazy} from 'react';
+import Home from './components/home';
+// import Shop from './components/shop';
+// import About from './components/about';
+// import Contact from './components/contact';
+import {Routes, Route, BrowserRouter as Router} from "react-router-dom"
+
+const Shop = lazy(() => import("./components/shop"))
+const Contact = lazy(() => import("./components/contact"))
+const About = lazy(() => import("./components/about"))
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li><a class="active" href="/">Home</a></li>
+        <li><a href="/shop">Shop</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
     </div>
+    <Routes>
+      <Route path="/" element={<Home></Home>}/>
+      <Route path="/shop" element={<Shop></Shop>}/>
+      <Route path="/contact" element={<Contact></Contact>}/>
+      <Route path="/about" element={<About></About>}/>
+    </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/shop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>I am Shop Page</div>
+  )
+}
+
+export default Shop
